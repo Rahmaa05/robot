d08d0504fbe1961d3d5e5079305eb1e7d6b2cdc2
Putrawin23181 <149153022+Putrawin23181@users.noreply.github.com>
2023-11-16 23:46:59 +0700

    Update robot.hurdle4.py

--- robot.hurdle4.py
+++ robot.hurdle4.py
@@ -1,1 +1,21 @@
+def kanan():
+    turn_left()
+    turn_left()
+    turn_left()
+def lompat():
+    while front_is_clear():
+        move()
+        if at_goal():
+            done()
+    turn_left()
+    while wall_on_right():
+        move()
+    kanan()
+    move()
+    kanan()
+    while front_is_clear():
+        move()
+    turn_left()
+while not at_goal():
+    lompat()
 
