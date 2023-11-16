68f785979e0883668d8045394fa07580f36fe90b
Rahmaa05 <149478588+Rahmaa05@users.noreply.github.com>
2023-11-16 22:08:53 +0700

    Add files via upload
    hurdle 1,2,3

---
+++ robot.hurdle3.py
@@ -0,0 +1,23 @@
+def lurus():
+    while front_is_clear():
+        move()
+        if at_goal():
+            done()
+    lompat()
+
+def kanan():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def lompat():
+    turn_left()
+    move()
+    kanan()
+    move()
+    kanan()
+    move()
+    turn_left()
+    lurus()
+lurus()
+lompat()
